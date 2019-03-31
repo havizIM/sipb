1e399c52d4fc3a21ed438320f4b5cc3b9c8093f6
BadZack08 <mzackyr@gmail.com>
2019-04-01 03:30:40 +0700

    Update Zacky

--- application/views/login.php
+++ application/views/login.php
@@ -17,6 +17,23 @@
 
   <link href="<?= base_url(''); ?>assets/dist/css/style.min.css" rel="stylesheet">
 
+  <script src="<?= base_url(''); ?>assets/node_modules/jquery/jquery-3.2.1.min.js"></script>
+
+  <script type="text/javascript">
+
+    // function cek_auth(){
+    //   var session = localStorage.getItem('session');
+    //   var auth = JSON.parse(session);
+    //
+    //   if (session) {
+    //     window.location.replace('<?= base_url() ?>'+auth.hak_akses+'/')
+    //   };
+    // };
+    //
+    // cek_auth();
+    //
+  </script>
+
   <style media="screen">
 
     .btn-show-pass {
@@ -64,35 +81,40 @@
 
 <body class="skin-default card-no-border">
 
-  <section id="wrapper" class="login-register login-sidebar" style="background-image:url('<?= base_url(''); ?>assets/images/background/login-register.jpg');">
+  <section id="wrapper" class="login-register login-sidebar" style="background-image:url('<?= base_url(''); ?>assets/images/background-login.jpg');">
     <div class="login-box card">
       <div class="card-body">
-        <form class="form-horizontal form-material text-center" id="loginform" action="index.html">
-          <a href="javascript:void(0)" class="db"><img src="<?= base_url(''); ?>assets/images/logo-icon.png" alt="Home" /><br/><img src="<?= base_url(''); ?>assets/images/logo-text.png" alt="Home" /></a>
+        <form class="form-horizontal form-material text-center" id="form_login" action="index.html">
+          <a href="javascript:void(0)" class="db"><img src="<?= base_url(''); ?>assets/images/logo.png" alt="Home" style="width: 50%;"></a>
           <div class="form-group m-t-40">
             <div class="col-xs-12">
-              <input class="form-control" id="username" type="text" required="" placeholder="Username">
+              <input class="form-control" id="username" name="username" type="text" required="" placeholder="Username">
             </div>
           </div>
           <div class="form-group">
             <div class="col-xs-12">
-              <span class="btn-show-pass">
-                <i class="fa fa-fw fa-eye" style="margin-bottom: 240px;"></i>
-              </span>
-              <input class="form-control" id="password" type="password" required="" placeholder="Password">
+              <input class="form-control" id="password" name="password" type="password" required="" placeholder="Password">
+            </div>
+          </div>
+          <div class="form-group row">
+            <div class="col-md-12">
+              <div class="d-flex no-block align-items-center">
+                <div class="custom-control custom-checkbox">
+                  <input type="checkbox" class="custom-control-input btn_show" id="customCheck1">
+                  <label class="custom-control-label" for="customCheck1">Lihat Password</label>
+                </div>
+              </div>
             </div>
           </div>
           <div class="form-group text-center m-t-20">
             <div class="col-xs-12">
-              <button class="btn btn-info btn-lg btn-block text-uppercase btn-rounded" type="submit">Log In</button>
+              <button class="btn btn-info btn-lg btn-block text-uppercase btn-rounded" id="btn_login" type="submit">Masuk</button>
             </div>
           </div>
         </form>
       </div>
     </div>
   </section>
-
-  <script src="<?= base_url(''); ?>assets/node_modules/jquery/jquery-3.2.1.min.js"></script>
 
   <script src="<?= base_url(''); ?>assets/node_modules/popper/popper.min.js"></script>
 
@@ -102,18 +124,47 @@
 
     $(document).ready(function(){
 
-      var showPass = 0;
-      $('.btn-show-pass').on('click', function(){
-        if(showPass == 0) {
-          $(this).next('input').attr('type', 'text');
-          $(this).find('i').removeClass('fa-eye');
-          $(this).find('i').addClass('fa-eye-slash');
-          showPass = 1;
+      $('.btn_show').on('change', function(){
+        var checked = $(this).prop('checked');
+
+        if (checked) {
+          $('#password').attr('type', 'text');
         } else {
-          $(this).next('input').attr('type', 'password');
-          $(this).find('i').addClass('fa-eye');
-          $(this).find('i').removeClass('fa-eye-slash');
-          showPass = 0;
+          $('#password').attr('type', 'password');
+        }
+      });
+
+      $('#form_login').on('submit', function(e){
+        e.preventDefault();
+
+        var username = $('#username').val();
+        var password = $('#password').val();
+
+        if(username === '' || password === ''){
+          alert('Username atau Password tidak boleh kosong');
+        } else {
+          $.ajax({
+            url: '<?= base_url('api/auth/login_user'); ?>',
+            type: 'POST',
+            dataType: 'JSON',
+            beforeSend: function(){
+              $('#btn_login').addClass('disabled').attr('disabled', 'disabled').html('<i class="fa fa-refresh fa-spin"></i>');
+            },
+            data: $('#form_login').serialize(),
+            success: function(response){
+              if(response.status === 200){
+                localStorage.setItem('sipb', JSON.stringify(response.data));
+                window.location.replace('<?= base_url() ?>'+response.data.hak_akses+'/')
+              } else {
+                alert(response.message);
+              }
+              $('#btn_login').removeClass('disabled').removeAttr('disabled', 'disabled').text('Masuk');
+            },
+            error: function(){
+              alert(response.message);
+              $('#btn_login').removeClass('disabled').removeAttr('disabled', 'disabled').text('Masuk');
+            }
+          });
         }
       });
 
